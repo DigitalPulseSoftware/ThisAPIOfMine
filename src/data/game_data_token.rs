02fc84475f7f4d39d69094611653094c38146933
hanako-eo <enuxura@gmail.com>
2024-08-26 16:59:28 +0200

    Add inline and assert into `GameDataToken`

--- src/data/game_data_token.rs
+++ src/data/game_data_token.rs
@@ -14,22 +14,31 @@ pub struct GameDataToken {
 }
 
 impl GameDataToken {
+    #[inline]
     pub fn new_access(player_db_id: i32, player_uuid: Uuid, duration: Duration) -> Self {
         Self::new("access", player_db_id, player_uuid, duration)
     }
 
+    #[inline]
     pub fn new_refresh(player_db_id: i32, player_uuid: Uuid, duration: Duration) -> Self {
         Self::new("refresh", player_db_id, player_uuid, duration)
     }
 
     fn new(token_type: &str, player_db_id: i32, player_uuid: Uuid, duration: Duration) -> Self {
+        debug_assert!(valid_token_type(token_type));
+
         let now = jsonwebtoken::get_current_timestamp();
         Self {
             player_db_id,
             player_uuid,
             exp: now + duration.as_secs(),
             iat: now,
-            sub: token_type.to_owned(),
+            sub: token_type.to_string(),
         }
     }
+}
+
+#[cfg(debug_assertions)]
+fn valid_token_type(tt: &str) -> bool {
+    tt == "access" || tt == "refresh"
 }
